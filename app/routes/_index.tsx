bb1940bf690101f38e3f6bfcceebe1badcb6063b
coji <coji@techtalk.jp>
2023-11-16 09:48:19 +0900

    Add Tailwind CSS and Framer Motion

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
+import { motion } from 'framer-motion'
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,32 +10,17 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <ul className="bg-red-200">
+        <motion.li animate={{ x: 100 }}>1</motion.li>
+        <motion.li animate={{ y: 100 }}>2</motion.li>
+        <motion.li animate={{ rotate: 360 }}>3</motion.li>
+        <motion.li className="cursor-pointer" whileHover={{ scale: 5 }}>
+          4
+        </motion.li>
+        <motion.li drag dragConstraints={{ left: 100, right: 100 }}>
+          5
+        </motion.li>
       </ul>
     </div>
   )
